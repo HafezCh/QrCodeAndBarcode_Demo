f9e1c8a2ac5f52732322389bca84e42ae7fe519f
Hafez <hafez.cheraghi1382@gmail.com>
2022-11-18 10:19:08 +0330

    QR Code Generator Implemented

--- QrCodeAndBarcode_Demo.web/Controllers/HomeController.cs
+++ QrCodeAndBarcode_Demo.web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QrCodeAndBarcode_Demo.web.Models;
+using QRCoder;
 
 namespace QrCodeAndBarcode_Demo.web.Controllers
 {
@@ -8,6 +10,47 @@ namespace QrCodeAndBarcode_Demo.web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("GenerateQrCode")]
+        public IActionResult GenerateQrCode()
+        {
+            return View();
+        }
+
+        [HttpPost("GenerateQrCode")]
+        public IActionResult GenerateQrCode(GenerateQrCodeModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            try
+            {
+                using MemoryStream memoryStream = new MemoryStream();
+                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+                QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(model.QrCodeText, QRCodeGenerator.ECCLevel.Q);
+
+                PngByteQRCode qRCode = new PngByteQRCode(qRCodeData);
+                byte[] qRCodeByte = qRCode.GetGraphic(20);
+
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/QrCodes");
+                if (Directory.Exists(path) == false)
+                    Directory.CreateDirectory(path);
+
+                var fileName = $"{Guid.NewGuid()}.png";
+                string filePath = Path.Combine(path, fileName);
+
+                using Stream stream = System.IO.File.Create(filePath);
+                stream.Write(qRCodeByte);
+
+                ViewBag.QrCode = fileName;
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
---
+++ QrCodeAndBarcode_Demo.web/Models/GenerateQrCodeModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QrCodeAndBarcode_Demo.web.Models;
+
+public class GenerateQrCodeModel
+{
+    [Display(Name = "Enter Qr Code Text")]
+    public string QrCodeText { get; set; }
+}
